5f5074dd79a8b0fca5583f4692779bbdf6de5915
MahmoudYaser <36421067+MahmoudYaser@users.noreply.github.com>
2018-02-19 18:24:05 +0200

    Add files via upload

---
+++ code_sheet_2_Q6.ino
@@ -0,0 +1,36 @@
+const int D1=13;
+const int D2=12;
+const int D3=11;
+const int LDR1=A0;
+const int LDR2=A1;
+const int LDR3=A2;
+int tourch1,tourch2,tourch3;
+
+void setup(){
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D3,OUTPUT);
+}
+
+void loop(){
+  tourch1 = analogRead(LDR1);
+  tourch2 = analogRead(LDR2);
+  tourch3 = analogRead(LDR3);
+
+  if(tourch1>tourch2 && tourch1>tourch3){
+    digitalWrite(D1,HIGH);
+    digitalWrite(D2,LOW);
+    digitalWrite(D3,LOW);
+  }
+   else if(tourch2>tourch1 && tourch2>tourch3){
+    digitalWrite(D1,LOW);
+    digitalWrite(D2,HIGH);
+    digitalWrite(D3,LOW);
+  }
+   else if(tourch3>tourch2 && tourch3>tourch1){
+    digitalWrite(D1,LOW);
+    digitalWrite(D2,LOW);
+    digitalWrite(D3,HIGH);
+  }
+}
+
